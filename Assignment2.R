6bc1075de0905757b52271ddaeebf3271ef1a179
sisalcactus <hsiangchang2000@gmail.com>
2023-06-12 19:39:00 -0400

    Fixed typos and made the language a tad clearer.

--- Assignment2.R
+++ Assignment2.R
@@ -9,11 +9,13 @@ if(is.numeric(as.numeric(value)) &     # this is added so we can check if the in
    
   nchar(as.character(value)) == 3 &    # this is added so we can check if the inputted number has exactly three digits
    
-  (as.numeric(value)) >= 0){           # this is added so we can check if the inputted number is a positive number
+  (as.numeric(value)) > 0){           # this is added so we can check if the inputted number is a positive number
  
      value <- as.integer(value)        # this is added so we can have the type of the inputted text as integer
   
-     third_digit <- value %% 10        # the third (rightmost) digit is simply the remainder when the user's inputted number is divided by 10 
+# next, three new variables were defined as we need to isolate each digit from the inputted number
+     
+     third_digit <- value %% 10        # the third (rightmost) digit of the inputted number is simply the remainder when the user's inputted number is divided by 10 
                                        # e.g., when 198 is divided by 10, the remainder is 8, coinciding with the rightmost digit
      
      first_digit <- floor(value/100)   # the first digit can be isolated by first dividing it by 100   
@@ -22,9 +24,10 @@ if(is.numeric(as.numeric(value)) &     # this is added so we can check if the in
                                        # e.g., if the inputted text is 198, then we get floor(198/100)
                                        # which is floor(1.98), which comes out to 1
      
-     second_digit <- (value - third_digit - first_digit*100)/10  # the middle digit can be isolated by removing the third digit (ones place), which we already isolated,
-                                                                 # and the first digit (times 100 to have it back in the hundreds place), which we also already isolated,
-                                                                 # from the user's inputted number, leaving us with just the digit in the tens place and a zero in the ones place
+     second_digit <- (value - third_digit - first_digit*100)/10  # the middle digit can be isolated by removing from the user's inputted number the following:
+                                                                 # a) the third digit (ones place), which we already isolated,
+                                                                 # and b) the product of the first digit and 100 (so the first digit is back in the hundreds place), which we also already isolated;
+                                                                 # this leaves us with just the digit in the tens place and a zero in the ones place
                                                                  # dividing this number by 10 isolates the middle digit
                                                                  # e.g., if the inputted text is 198, we'd remove 8 (third digit) and 100 (first digit times 100), leaving 90
                                                                  # then dividing it by 10 yields 9 (second digit)
@@ -36,7 +39,7 @@ if(is.numeric(as.numeric(value)) &     # this is added so we can check if the in
     
      } else {                                                                           
     
-    print(paste(value, "is a three-digit positive number but not a narcissitic number.")) # this is added to inform the user their number is not a narcissistic number
+    print(paste(value, "is a three-digit positive number but not a narcissistic number.")) # this is added to inform the user their number is not a narcissistic number
     
      }                                          # this inner if-else relates to the case where the user's inputted number has been verified as being numeric
                                                 # (and is a positive three-digit number)
